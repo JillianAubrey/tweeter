e2c053c497f2ff07f42ccf2ca67ea7ba1d731db5
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-12 23:07:26 +0000

    Create client function createTweetElement to create a new tweet from an object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,57 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+ $(document).ready(() => {
+  
+   const createTweetElement = function(tweet) {
+    const $tweet = $(`<article class='tweet'>`);
+    const $tweetHeader = $(
+      `<header>
+        <div class="user">
+          <img src="${tweet.user.avatars}">
+          <p>${tweet.user.name}</p>
+        </div>
+        <p class="handle">${tweet.user.handle}</p>
+      </header>`
+    );
+    const $tweetContent = $(`<p>`);
+    $tweetContent.text(tweet.content.text)
+    const $tweetFooter = $(
+      `<footer>
+        <p>10 days ago</p>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <div class="likes">
+            <i class="fa-solid fa-heart"></i>
+            <p>1</p>
+          </div>
+        </div>
+      </footer>`
+    );
+    
+    $tweet.append($tweetHeader);
+    $tweet.append($tweetContent);
+    $tweet.append($tweetFooter);
 
+    return $tweet;
+   }
+   
+   const tweetData = {
+     "user": {
+       "name": "Newton",
+       "avatars": "https://i.imgur.com/73hZDYK.png",
+         "handle": "@SirIsaac"
+       },
+     "content": {
+         "text": "If I have seen further it is by standing on the shoulders of giants"
+       },
+     "created_at": 1461116232227
+   }
+   
+   const $tweet = createTweetElement(tweetData);
+   
+   // Test / driver code (temporary)
+   console.log($tweet); // to see what it looks like
+   $('#tweets-container').append($tweet); 
+ })
