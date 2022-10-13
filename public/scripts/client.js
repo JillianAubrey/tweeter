6e12cebf1cedc43df8d918ccb2d7f6aa3a86f662
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-10-13 02:34:21 +0000

    Add form validation to submission of new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,40 +5,32 @@
  */
 
 $(document).ready(() => {
+  const loadTweets = function() {
+    const url = '/tweets';
+    const method = 'GET';
+    $.ajax({ url, method }).done(tweets => {
+      renderTweets(tweets);
+    })
+  }
+  loadTweets();
+  
   $('.new-tweet form').submit(function(event) {
     event.preventDefault();
     const data = $(this).serialize();
     const url = '/tweets';
     const method = 'POST';
-    $.ajax({ url, method, data, }).done(() => {
-      console.log('Form posted!');
+    const tweetText = data.slice('text='.length);
+    const maxTweetLength = 140;
+    if (!tweetText){
+      return alert('Tweet cannot be empty!');
+    }
+    if (tweetText.length > maxTweetLength) {
+      return alert(`Tweet cannot be more than ${maxTweetLength} characters!`);
+    }
+    $.ajax({ url, method, data }).done((response) => {
+      console.log(response);
     });
   })
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
 
   const renderTweets = function(tweets) {
     tweets.forEach(tweet => {
@@ -80,6 +72,4 @@ $(document).ready(() => {
 
     return $tweet;
   };
-   
-  renderTweets(data);
 });
